14ac2c60c20b9d9368021dbd204b2a1980fb8bdf
Reto Stockli <reto.stockli@gmail.com>
2022-06-16 22:24:31 +0200

    update scripts

--- scripts/updaterasperry.sh
+++ scripts/updaterasperry.sh
@@ -4,30 +4,35 @@
 
 ssh du1  "cd xpcockpit/scripts; ./updatexpcockpit.sh"
 if [ $? != 0 ]
+then
     echo "error in updating du1"
     exit 1
 fi
 
 ssh du2  "cd xpcockpit/scripts; ./updatexpcockpit.sh"
 if [ $? != 0 ]
+then
     echo "error in updating du2"
     exit 2
 fi
 
 ssh eicas "cd xpcockpit/scripts; ./updatexpcockpit.sh"
 if [ $? != 0 ]
+then
     echo "error in updating eicas"
     exit 3
 fi
 
 ssh fmc1  "cd xpcockpit/scripts; ./updatexpcockpit.sh"
 if [ $? != 0 ]
+then
     echo "error in updating fmc1"
     exit 4
 fi
 
 #ssh fmc2  "cd xpcockpit/scripts; ./updatexpcockpit.sh"
 #if [ $? != 0 ]
+#then
 #    echo "error in updating fmc2
 #    exit 5
 #fi
