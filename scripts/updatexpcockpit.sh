cd4153269cdc9a71ccf13abfc2c70e2f10717ee0
Reto Stockli <reto.stockli@gmail.com>
2022-06-16 22:22:09 +0200

    update scripts

--- scripts/updatexpcockpit.sh
+++ scripts/updatexpcockpit.sh
@@ -4,13 +4,33 @@
 
 cd ~/xpcockpit
 git pull origin B737
+if [ $? != 0 ]
+    echo "error in pulling git branch"
+    exit 1
+fi
 cd xpopengc
 make 
+if [ $? != 0 ]
+    echo "error in make xpopengc"
+    exit 2
+fi
 make install
+if [ $? != 0 ]
+    echo "error in make install xpopengc"
+    exit 3
+fi
 cd ..
 cd xppi
 make
+if [ $? != 0 ]
+    echo "error in make xppi"
+    exit 4
+fi
 make install
+if [ $? != 0 ]
+    echo "error in make install xppi"
+    exit 5
+fi
 cd raspian
 cp *.service ~/.config/systemd/user/
 
