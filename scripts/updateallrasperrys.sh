c2cbcbe3b904cf6c00fee4113bdbc9a6cd9aa255
Reto Stockli <reto.stockli@gmail.com>
2022-10-15 15:02:24 +0200

    updatescripts modified

--- scripts/updateallrasperrys.sh
+++ scripts/updateallrasperrys.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-xterm -e "./updaterasperry.sh du1; $SHELL"
-xterm -e "./updaterasperry.sh du2; $SHELL"
-xterm -e "./updaterasperry.sh eicas; $SHELL"
-xterm -e "./updaterasperry.sh fmc1; $SHELL"
+xterm -e "./updaterasperry.sh du1; $SHELL" &
+xterm -e "./updaterasperry.sh du2; $SHELL" &
+xterm -e "./updaterasperry.sh eicas; $SHELL" &
+xterm -e "./updaterasperry.sh fmc1; $SHELL" &
 
