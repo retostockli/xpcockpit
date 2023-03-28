e76c612e36e5c74cfdaa43e1fd6812ea98fb422e
Reto Stockli <reto.stockli@gmail.com>
2023-03-28 22:43:48 +0200

    projector update

--- projection/warping.R
+++ projection/warping.R
@@ -6,12 +6,14 @@ d2r = pi/180.
 r2d = 180./pi
 
 ## Dimensions (in cm)
-R = 169.5   # Screen Radius
-d_0 = 27.0   # Distance of Projector lens from center of cylinder (positive is towards screen)
+R = 169.0   # Screen Radius
+d_0 = 30.0   # Distance of Projector focal point from center of cylinder (positive is towards screen)
+## Projector focal point may be behind projector lens. For me it is around 2-4 cm behind
+## as it seems. No documentation found.
 tr = 0.49   # Projector Throw ratio
 h_0 = 15.0   # lower height of image above center of lens when projected on planar screen from untilted projector
 
-epsilon = 7.5  # degrees projector tilt (downwards for regular and upwards for ceiling mount)
+epsilon = 5.75  # degrees projector tilt (downwards for regular and upwards for ceiling mount)
 #epsilon = 0.0  # degrees projector tilt (downwards for regular and upwards for ceiling mount)
 
 nmon = 1  # number of monitors
@@ -30,25 +32,11 @@ beta = atan(1.0/(2.0*tr))*r2d
 beta1 = 180.-beta
 delta = asin(d_0/R*sin(beta1*d2r))*r2d
 gamma = 180.-beta1-delta
-R1 = R*sin(gamma*d2r)/sin(beta1*d2r)
-d_1 = R1*cos(beta*d2r)
-w_2 = R1*sin(beta*d2r)
+R_1 = R*sin(gamma*d2r)/sin(beta1*d2r)
+d_1 = R_1*cos(beta*d2r)
+w_2 = R_1*sin(beta*d2r)
 w = 2*w_2    # planar image width at screen distance
 h = w / ar  # planar image height at screen distance
-
-## Calculate Highest and Lowest Height and corresponding elevation angles
-## for trapezoid correction
-if (epsilon != 0.0) {
-    h_H = h_0 + h
-    h_L = h_0
-    alpha_H = atan(h_H/d_1)*r2d
-    alpha_L = atan(h_L/d_1)*r2d
-    g_H = tan((alpha_H-epsilon)*d2r)*d_1  ## Highest Image height with Tilt
-    g_L = tan((alpha_L-epsilon)*d2r)*d_1  ## Lowest Image height with Tilt
-    h_k = g_H - g_L
-}
-    
-##print(paste(w,h))
 
 
 ## set up plot
@@ -107,13 +95,6 @@ for (gx in (1:ngx)) {
     z_2 = h_0 + py/(ny-1) * h
     alpha = atan(z_2/d_r)*r2d
 
-    if (epsilon != 0.0) {
-        ## rotate elevation angle by tilt
-        alpha_k = alpha - epsilon
-    } else {
-        alpha_k = alpha
-    }
-
     z_c = tan(alpha_k*d2r) * d_d
     phi = atan(z_2/d_d)*r2d
     z_e = tan(phi*d2r) * d_r
@@ -126,11 +107,12 @@ for (gx in (1:ngx)) {
     ## so uncompress and untrapezoid the image pixel right in the first place and place them
     ## where they would be with a untilted projector
     if (epsilon != 0.0) {
-        u = d_1 / cos(alpha*d2r)   # image distance at line py when projector would be level
-        u_k = d_1 / cos((alpha-epsilon)*d2r)  # image distance at line py with tilt
+        alpha_0 = atan(z_2/d_1)*r2d  # elevation angle at center line of projection
+        u = d_1 / cos(alpha_0*d2r)   # image distance at line py when projector would be level
+        u_k = d_1 / cos((alpha_0-epsilon)*d2r)  # image distance at line py with tilt
 
         ex = (ex-nx/2.0)*u/u_k + nx/2.0  # correct keystone in horizontal pixels
-        ey = ey * h / h_k  # correct keystone in vertical pixels
+        ey = ey * u / u_k  # correct keystone in vertical pixels
     }
 
     ## Apply Cylindrical Warping Correction
