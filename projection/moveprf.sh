4ea5cff9f9c692f8de821fe08883dcfbe3e8ac4b
Reto Stockli <reto.stockli@gmail.com>
2023-04-13 23:43:43 +0200

    projection

--- projection/moveprf.sh
+++ projection/moveprf.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-cat prepend_one_screen.txt grid.txt > X-Plane\ Window\ Positions.prf 
 cp X-Plane\ Window\ Positions.prf ~/X-Plane\ 11/Output/preferences/
--- projection/warping.R
+++ projection/warping.R
@@ -1,3 +1,12 @@
+## GTX970 Output Numbering
+## DVI0 (DVI-I-1): 0
+## HDMI/HP (DP-1): 1
+## DVI1 (DVI-D-0): 2
+## HDMI (HDMI-0): 3
+
+## X-Plane 11: need to activate VULKAN in Graphics Settings
+## However: With Vulkan, Cylindrical projection has limited rendering in corners
+
 ## Graphics
 do.plot = FALSE
 
@@ -13,10 +22,18 @@ d_0 = 30.0   # Distance of Projector focal point from center of cylinder (positi
 tr = 0.49   # Projector Throw ratio
 h_0 = 15.0   # lower height of image above center of lens when projected on planar screen from untilted projector
 
-epsilon = 5.75  # degrees projector tilt (downwards for regular and upwards for ceiling mount)
-#epsilon = 0.0  # degrees projector tilt (downwards for regular and upwards for ceiling mount)
+nmon = 4  # number of monitors
 
-nmon = 1  # number of monitors
+projection = c(FALSE,TRUE,TRUE,TRUE)
+blending = c(FALSE,TRUE,TRUE,TRUE)
+epsilon = c(0.0,0.0,5.75,0.0)
+lateral_offset = c(0.0,-65.0,0.0,65.0)
+vertical_offset = c(0.0,0.0,0.0,0.0)
+gridtest = FALSE # display grid test pattern
+forwin = TRUE  # create for windows or for linux
+
+
+outfile = "X-Plane Window Positions.prf"
 
 ## pixel dimensions of projector
 nx = 1920.0
@@ -25,7 +42,15 @@ ny = 1080.0
 ## number of x and y grid points
 ngx = 101
 ngy = 101
-    
+
+## Generate Output File and write header
+con <- file(outfile)
+open(con,open="w")
+writeLines("I",con)
+writeLines("10 Version",con)
+writeLines(paste("num_monitors",nmon),con)
+
+
 ## General Calculations independent of pixel position
 ar = nx / ny  # aspect ratio of projector image
 beta = atan(1.0/(2.0*tr))*r2d
@@ -46,106 +71,183 @@ if (do.plot) {
          xlim = c(0, nx), ylim = c(0, ny))
 }
 
-xdif = array(NA,dim=c(ngx,ngy))
-ydif = array(NA,dim=c(ngx,ngy))
+## reset projection grid
+xdif = array(0.0,dim=c(ngx,ngy))
+ydif = array(0.0,dim=c(ngx,ngy))
 
 ## loop through grid
 for (mon in (0:(nmon-1))) {
-for (gy in (1:ngy)) {
-for (gx in (1:ngx)) {
 
-    ## Calculate Pixel position
-    px = nx * (gx-1) / (ngx-1)
-    py = ny * (gy-1) / (ngy-1)
+    if (projection[mon+1]) {
+        for (gy in (1:ngy)) {
+            for (gx in (1:ngx)) {
 
- 
-    ## Calculate horizontal position on hypothetical
-    ## planar screen in distance d_1 from projector
-    ## and respective horizontal projection angle
-    a = w * (px - (nx-1.0)/2.0) / (nx-1.0)
-    theta = atan(a/d_1)*r2d
-    
-    ## Calculate intersection of projected line with the cylindrical screen
-    ## behind hypothetical planar screen. Cylinder is in (0,0) position
-    ## https://mathworld.wolfram.com/Circle-LineIntersection.html
-    x_1 = d_0
-    x_2 = d_1+d_0
-    y_1 = 0
-    y_2 = a
-    d_x = x_2-x_1
-    d_y = y_2-y_1
-    d_r = sqrt(d_x^2+d_y^2)
-    D   = x_1*y_2-x_2*y_1
-    DSCR = R^2*d_r^2 - D^2  # Discriminant: if > 0 (true for our problem: intersection)
-    
-    if (d_y < 0) {
-        sgn_dy = -1.0
+                ## Calculate Pixel position
+                px = nx * (gx-1) / (ngx-1)
+                py = ny * (gy-1) / (ngy-1)
+
+                
+                ## Calculate horizontal position on hypothetical
+                ## planar screen in distance d_1 from projector
+                ## and respective horizontal projection angle
+                a = w * (px - (nx-1.0)/2.0) / (nx-1.0)
+                theta = atan(a/d_1)*r2d
+                
+                ## Calculate intersection of projected line with the cylindrical screen
+                ## behind hypothetical planar screen. Cylinder is in (0,0) position
+                ## https://mathworld.wolfram.com/Circle-LineIntersection.html
+                x_1 = d_0
+                x_2 = d_1+d_0
+                y_1 = 0
+                y_2 = a
+                d_x = x_2-x_1
+                d_y = y_2-y_1
+                d_r = sqrt(d_x^2+d_y^2)
+                D   = x_1*y_2-x_2*y_1
+                DSCR = R^2*d_r^2 - D^2  # Discriminant: if > 0 (true for our problem: intersection)
+                
+                if (d_y < 0) {
+                    sgn_dy = -1.0
+                } else {
+                    sgn_dy = 1.0
+                }
+                x_c = (D*d_y + sgn_dy + d_x * sqrt(DSCR))/d_r^2
+                y_c = (-D*d_x + d_y * sqrt(DSCR))/d_r^2
+                
+                ## now calculate how much we need to shift pixel coordinates to match
+                ## cylindrical screen again
+                d_2 = sqrt((x_c-x_2)^2 + (y_c-y_2)^2)  # overshoot distance (should be 0 at screen edges)
+                d_d = d_r + d_2 # total horizontal distance from projector to cylindrical screen
+
+                ## Calculate Elevation angle of pixel
+                z_2 = h_0 + py/(ny-1) * h
+                alpha = atan(z_2/d_r)*r2d
+                
+                z_c = tan(alpha*d2r) * d_d
+                phi = atan(z_2/d_d)*r2d
+                z_e = tan(phi*d2r) * d_r
+
+                ex = px
+                ey = py
+                
+                ## Apply Keystone Correction
+                ## image gets compressed in both horizontal and vertical dimensions with projector tilt
+                ## so uncompress and untrapezoid the image pixel right in the first place and place them
+                ## where they would be with a untilted projector
+                if (epsilon[mon+1] != 0.0) {
+                    alpha_0 = atan(z_2/d_1)*r2d  # elevation angle at center line of projection
+                    u = d_1 / cos(alpha_0*d2r)   # image distance at line py when projector would be level
+                    u_k = d_1 / cos((alpha_0-epsilon[mon+1])*d2r)  # image distance at line py with tilt
+
+                    ex = (ex-nx/2.0)*u/u_k + nx/2.0  # correct keystone in horizontal pixels
+                    ey = ey * u / u_k  # correct keystone in vertical pixels
+                }
+
+                ## Apply Cylindrical Warping Correction
+                ex = ex
+                ey = (z_e - h_0) / h * (ny-1) - py + ey
+                
+                ##print(paste(ex,ey))
+                if (do.plot) {
+                    points(ex,ey)
+                }
+                ##print(paste(px,py))
+
+                xdif[gx,gy] = ex - px
+                ydif[gx,gy] = ey - py
+            }
+        }
+
+    }
+
+    ## write header per monitor
+    if (mon == 0) {
+        writeLines(paste("monitor/",mon,"/m_window_idx 0",sep=""),con)
     } else {
-        sgn_dy = 1.0
+        writeLines(paste("monitor/",mon,"/m_window_idx -1",sep=""),con)
     }
-    x_c = (D*d_y + sgn_dy + d_x * sqrt(DSCR))/d_r^2
-    y_c = (-D*d_x + d_y * sqrt(DSCR))/d_r^2
-    
-    ## now calculate how much we need to shift pixel coordinates to match
-    ## cylindrical screen again
-    d_2 = sqrt((x_c-x_2)^2 + (y_c-y_2)^2)  # overshoot distance (should be 0 at screen edges)
-    d_d = d_r + d_2 # total horizontal distance from projector to cylindrical screen
+    if (forwin) {
+        writeLines(paste("monitor/",mon,"/m_monitor ",mon,sep=""),con)
+    } else {
+        writeLines(paste("monitor/",mon,"/m_monitor 0",sep=""),con)
+    }
+    writeLines(paste("monitor/",mon,"/m_window_bounds/0 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_window_bounds/1 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_window_bounds/2 ",nx,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_window_bounds/3 ",ny,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_arbitrary_bounds/0 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_arbitrary_bounds/1 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_arbitrary_bounds/2 ",nx,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_arbitrary_bounds/3 ",ny,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_bpp 24",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_refresh_rate 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_x_res_full ",nx,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_y_res_full ",ny,sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_is_fullscreen wmgr_mode_fullscreen",sep=""),con)
+    writeLines(paste("monitor/",mon,"/m_usage wmgr_usage_normal_visuals",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/window_2d_off 1",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/diff_FOV 0",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/FOVx_renopt ",formatC(gamma*2,digits=6,format = "f"),sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/FOVy_renopt ",formatC(gamma*2*9/16,digits=6,format = "f"),sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/os_x_rat 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/os_y_rat 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/off_vrt_deg ",formatC(vertical_offset[mon+1],digits=6,format = "f"),sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/off_lat_deg ",formatC(lateral_offset[mon+1],digits=6,format = "f"),sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/off_phi_deg 0.000000",sep=""),con)
+    if (gridtest) {
+        writeLines(paste("monitor/",mon,"/proj/grid_os_on_test 1",sep=""),con)
+    } else {
+        writeLines(paste("monitor/",mon,"/proj/grid_os_on_test 0",sep=""),con)
+    }
+    if (projection[mon+1]) {
+        writeLines(paste("monitor/",mon,"/proj/grid_os_on_render 1",sep=""),con)
+    } else {
+        writeLines(paste("monitor/",mon,"/proj/grid_os_on_render 0",sep=""),con)
+    }
+    writeLines(paste("monitor/",mon,"/proj/grid_os_drag_dim_i 4",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/grid_os_drag_dim_j 4",sep=""),con)
 
-    ## Calculate Elevation angle of pixel
-    z_2 = h_0 + py/(ny-1) * h
-    alpha = atan(z_2/d_r)*r2d
-    
-    z_c = tan(alpha*d2r) * d_d
-    phi = atan(z_2/d_d)*r2d
-    z_e = tan(phi*d2r) * d_r
-
-    ex = px
-    ey = py
-    
-    ## Apply Keystone Correction
-    ## image gets compressed in both horizontal and vertical dimensions with projector tilt
-    ## so uncompress and untrapezoid the image pixel right in the first place and place them
-    ## where they would be with a untilted projector
-    if (epsilon != 0.0) {
-        alpha_0 = atan(z_2/d_1)*r2d  # elevation angle at center line of projection
-        u = d_1 / cos(alpha_0*d2r)   # image distance at line py when projector would be level
-        u_k = d_1 / cos((alpha_0-epsilon)*d2r)  # image distance at line py with tilt
-
-        ex = (ex-nx/2.0)*u/u_k + nx/2.0  # correct keystone in horizontal pixels
-        ey = ey * u / u_k  # correct keystone in vertical pixels
+    ## write grid per monitor
+    if (projection[mon+1]) {
+        for (gx in (1:ngx)) {
+            for (gy in (1:ngy)) {
+                writeLines(paste("monitor/",mon,"/proj/grid_os_x/",gx-1,"/",gy-1," ",
+                                 formatC(xdif[gx,gy],digits=6,format = "f"),sep=""),con)
+            }
+        }
+        for (gx in (1:ngx)) {
+            for (gy in (1:ngy)) {
+                writeLines(paste("monitor/",mon,"/proj/grid_os_y/",gx-1,"/",gy-1," ",
+                                 formatC(ydif[gx,gy],digits=6,format = "f"),sep=""),con)
+            }
+        }
     }
 
-    ## Apply Cylindrical Warping Correction
-    ex = ex
-    ey = (z_e - h_0) / h * (ny-1) - py + ey
+    ## write blending per monitor
+    if (blending[mon+1]) {
+        writeLines(paste("monitor/",mon,"/proj/gradient_on 1",sep=""),con)
+    } else {
+        writeLines(paste("monitor/",mon,"/proj/gradient_on 0",sep=""),con)
+    }
         
-    ##print(paste(ex,ey))
-    if (do.plot) {
-        points(ex,ey)
-    }
-    ##print(paste(px,py))
-
-    xdif[gx,gy] = ex - px
-    ydif[gx,gy] = ey - py
-}
-}
-sink("grid.txt")
-for (gy in (1:ngy)) {
-    for (gx in (1:ngx)) {
-        cat(paste("monitor/",mon,"/proj/grid_os_x/",gx-1,"/",gy-1," ",
-                  formatC(xdif[gx,gy],digits=6,format = "f"),"\n",sep=""))
-    }
-}
-for (gy in (1:ngy)) {
-    for (gx in (1:ngx)) {
-        cat(paste("monitor/",mon,"/proj/grid_os_y/",gx-1,"/",gy-1," ",
-                  formatC(ydif[gx,gy],digits=6,format = "f"),"\n",sep=""))
-    }
-}
-sink()
-
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_top/0 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_top/1 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_ctr/0 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_ctr/1 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_bot/0 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_width_bot/1 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/0/0 1.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/0/1 0.650000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/0/2 0.330000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/0/3 0.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/1/0 1.000000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/1/1 0.650000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/1/2 0.330000",sep=""),con)
+    writeLines(paste("monitor/",mon,"/proj/gradient_alpha/1/3 0.000000",sep=""),con)
+    
 }
 
+close(con)
 
 
-#browser()
+                                        #browser()
