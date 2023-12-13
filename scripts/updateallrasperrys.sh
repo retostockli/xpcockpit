d24b40d53796c6ed2985a44562bf73091a170dff
Reto Stockli <reto@bluemarble.ch>
2023-12-13 21:40:52 +0100

    throttle fix

--- scripts/updateallrasperrys.sh
+++ scripts/updateallrasperrys.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 xterm -e "./updaterasperry.sh du1; $SHELL" &
+sleep 2
 xterm -e "./updaterasperry.sh du2; $SHELL" &
+sleep 2
 xterm -e "./updaterasperry.sh eicas; $SHELL" &
+sleep 2
 xterm -e "./updaterasperry.sh fmc1; $SHELL" &
 
