7f66b33b9f88ec9109b41f103a5847933458bdcf
Reto Stockli <reto.stockli@gmail.com>
2024-01-15 23:15:05 +0100

    IRS Display Panel

--- xpsismo/src/b737_aftoverhead.c
+++ xpsismo/src/b737_aftoverhead.c
@@ -30,6 +30,12 @@
 #include "serverdata.h"
 #include "b737_aftoverhead.h"
 
+#define COUNT_MAX 5
+
+int col;
+int counter;
+
+
 void b737_aftoverhead(void)
 {
   int card = 4; /* SISMO card according to ini file */
@@ -41,17 +47,21 @@ void b737_aftoverhead(void)
   int display;
   int i;
   int outvalue;
+  int i0;
+  int o0;
 
 
+
+  /*** IRS DISPLAY PANEL ***/
+
+  i0 = 0;
+  o0 = 0;
+  
   temp = 1;
 
   int *invalue = link_dataref_int("xpserver/test");
-  
-  outvalue = 1;
-  ret = digital_output(card,0,&outvalue);
-  outvalue = 1;
-  ret = digital_output(card,1,&outvalue);
 
+  /* BRT Potentiometer driving 7 segment brightness */
   i=0;
   ret = analog_input(card,i,&fvalue,0.0,10.0);
   if (ret == 1) {
@@ -59,16 +69,53 @@ void b737_aftoverhead(void)
     printf("Analog Input %i changed to: %f \n",i,fvalue);
   }
 
-  i=0;
-  ret = digital_input(card, i, invalue, 0);
-  if (ret == 1) {
-    /* ret is 1 only if input has changed */
-    printf("Digital Input %i changed to: %i \n",i,*invalue);
-  }
+  /* ENT Key Light */
+  outvalue = 1;
+  ret = digital_output(card,o0+6,&outvalue);
 
-  display = 88888;
+  /* CLR Key Light */
+  outvalue = 1;
+  ret = digital_output(card,o0+7,&outvalue);
+
+  /* 12 key keyboard: Matrix arrangement */
+  if ((col<0) || (col>2)) col = 0;
+  if ((counter<0) || (counter>COUNT_MAX)) counter = 0;
+
+  //col = 0;
+  
+  //if (counter == 0) {
+    for (int c=0;c<3;c++) {
+      if (c==col) { outvalue = 0; } else { outvalue = 1; }
+      ret = digital_output(card,o0+c,&outvalue);
+    }
+    //}
+
+  //printf("%i %i \n",col,counter);
+  
+  //if (counter == COUNT_MAX) {
+    i0=8;
+    for (int r=0;r<4;r++) {
+      *invalue = 0;
+      ret = digital_input(card, i0+r, invalue, 0);
+      if (ret == 1) {
+	/* ret is 1 only if input has changed */
+	printf("Column %i Row %i changed to: %i \n",col,r,*invalue);
+      }
+    }
+    //}
+
+  if (counter == COUNT_MAX) {
+    col++;
+    if (col>2) col = 0;
+    counter = 0;
+  }
+  counter++;
+    
+  display = 888888;
+  //display = 999999;
   
   /* set 7 segment displays 0-5 to the 5 digit value of the encoder with a decimal point at digit 2 */
-  ret = display_output(card, 0, 5, &display, 0, 15);
+  ret = display_output(card, 0, 6, &display, 0, 10);
+  ret = display_output(card, 8, 6, &display, 0, 10);
 
 }
