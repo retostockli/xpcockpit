d7e3b3afcb3631ec9cc4db78e59448494482fc0e
Reto Stockli <reto.stockli@gmail.com>
2024-07-11 21:23:44 +0200

    sync across systems

---
+++ scripts/makewinzip.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Create a Windows ZIP file for distribution
+# Include for now:
+# xpserver plugin 
+# opengc executable and shared files
+
+zipdir=xpcockpit-windows
+zipfile=xpcockpit-windows.zip
+
+
+mkdir ${zipdir}
+mkdir ${zipdir}/xpserver/
+mkdir ${zipdir}/xpopengc/
+
+cp -a ../xpserver/plugins ../${zipdir}/xpserver/
+cp -a ../xpopengc/bin ../${zipdir}/xpopengc/
+cp -a ../xpopengc/share ../${zipdir}/xpopengc/
+
+cp ../README_WINDOWS ../${zipdir}
+
+zip -r ../xpcockpit-windows.zip ../xpcockpit-windows
