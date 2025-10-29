a8955d226027f826fd737a3f854f9826d27a31a7
Reto Stockli <reto@bluemarble.ch>
2025-10-29 22:31:43 +0100

    ap ias mach

--- xpsismo/src/b737_mcp.c
+++ xpsismo/src/b737_mcp.c
@@ -674,8 +674,8 @@ void b737_mcp(void)
   if (ap_spd_show == 1) {
     if (*ap_spd_is_mach == 1) {
       float ftemp = *ap_ias * 100.0;
-      ret = display_outputf(card,3,4, ap_ias, -1, display_brightness);  // clear all digits
-      ret = display_outputf(card,5,2, &ftemp, 3, display_brightness); // mach spd in two first digits
+      ret = display_outputf(card,3,4, ap_ias, -1, zero);  // clear all digits
+      ret = display_outputf(card,3,3, &ftemp, 2, display_brightness); // mach spd in two first digits
     } else {
       ret = display_outputf(card,3,4, ap_ias, -1, display_brightness);
     }
